2ca74b884e740d284da56795c600b051476749db
Ilona Myroniuk <knopkakyrs@gmail.com>
2024-11-14 09:39:11 +0000

    finished responsive design for History page; fixed path for travel buttons at additional pages;

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 'use strict';
 
-const travelButtons = document.querySelectorAll("#travel-btn");
-console.log(travelButtons);
+const travelButtons = document.querySelectorAll("#travel-btn-main");
+const additionalTravelButtons = document.querySelectorAll("#travel-btn");
+// console.log(travelButtons);
 
 
 travelButtons.forEach(button => {
     button.onclick = function () {
         console.log(button);
         window.location.href = "./htmlPages/buyTrip.html";
+    };
+});
+additionalTravelButtons.forEach(button => {
+    button.onclick = function () {
+        console.log(button);
+        window.location.href = "../htmlPages/buyTrip.html";
     };
 });
